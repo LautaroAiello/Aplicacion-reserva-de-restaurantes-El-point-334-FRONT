10e56f21f5c587a61b8d2efc9d8de57cd156b660
Alejo Beliz <129092769+albelizGH@users.noreply.github.com>
2025-10-02 20:42:56 -0300

    home/cards-mocks

---
+++ src/app/cliente/components/simple-card/simple-card.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component, input, InputSignal, output } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-card',
+  imports: [],
+  templateUrl: './simple-card.html',
+  styleUrl: './simple-card.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SimpleCard {
+  public id: InputSignal<number> = input.required();
+  public imgUrl: InputSignal<string> = input.required();
+  public title: InputSignal<string> = input.required();
+  public subtitle: InputSignal<string> = input('');
+
+  public cardClicked = output<number>();
+  
+}
--- src/app/cliente/pages/home/home.ts
+++ src/app/cliente/pages/home/home.ts
@@ -1,10 +1,36 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SimpleCard } from '../../components/simple-card/simple-card';
 
 @Component({
   selector: 'app-cliente-home-page',
-  imports: [],
+  imports: [SimpleCard],
   templateUrl: './home.html',
   styleUrl: './home.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ClienteHomePage {}
+export class ClienteHomePage {
+  protected mockCards = [
+    {
+      id: 1,
+      imgUrl: 'https://picsum.photos/200/300',
+      title: 'Card 1',
+      subtitle: 'Subtitle 1',
+    },
+    {
+      id: 2,
+      imgUrl: 'https://picsum.photos/200/300',
+      title: 'Card 2',
+      subtitle: 'Subtitle 2',
+    },
+    {
+      id: 3,
+      imgUrl: 'https://picsum.photos/200/300',
+      title: 'Card 3',
+      subtitle: 'Subtitle 3',
+    },
+  ];
+
+  public cardClicked(id: number):void  {
+    console.log('Card clicked', id);
+  }
+}
