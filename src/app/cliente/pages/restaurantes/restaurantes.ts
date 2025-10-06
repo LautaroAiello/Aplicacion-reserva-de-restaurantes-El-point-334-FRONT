e3f3f5712529c47035f32613015f492f3d6c10e9
sabarettaGH <santinobaretta30@gmail.com>
2025-10-06 20:06:49 -0300

    add-agrugue buscador-con-filtro sobre pantalla de restaurantes

--- src/app/cliente/pages/restaurantes/restaurantes.ts
+++ src/app/cliente/pages/restaurantes/restaurantes.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RestauranteCard } from "./components/restaurante-card/restaurante-card";
+import { RestauranteCard } from './components/restaurante-card/restaurante-card';
 import { BuscadorConFiltro } from '../../components/buscador-con-filtro/buscador-con-filtro';
 
 @Component({
   selector: 'app-cliente-restaurantes-page',
-  imports: [RestauranteCard,BuscadorConFiltro],
+  imports: [RestauranteCard, BuscadorConFiltro],
   templateUrl: './restaurantes.html',
   styleUrl: './restaurantes.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
