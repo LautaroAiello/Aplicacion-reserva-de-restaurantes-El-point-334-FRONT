757afa8b3fb0d3aa210a7499cf7fd558b674e31d
sabarettaGH <santinobaretta30@gmail.com>
2025-10-06 20:31:07 -0300

    Fix

--- src/app/cliente/pages/restaurantes/restaurantes.ts
+++ src/app/cliente/pages/restaurantes/restaurantes.ts
@@ -9,4 +9,31 @@ import { BuscadorConFiltro } from '../../components/buscador-con-filtro/buscador
   styleUrl: './restaurantes.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ClienteRestaurantesPage {}
+export class ClienteRestaurantesPage {
+  protected restaurantesMock = [
+    {
+      nombreRestaurante: 'Restaurante 1',
+      imagenRestaurante: 'https://picsum.photos/400/300',
+      etiquetas: ['italiana', 'pasta'],
+      calificacion: 4.5,
+      direccion: 'Calle 1',
+      distancia: '1 km',
+    },
+    {
+      nombreRestaurante: 'Restaurante 2',
+      imagenRestaurante: 'https://picsum.photos/400/300',
+      etiquetas: ['mexicana', 'tacos'],
+      calificacion: 4.0,
+      direccion: 'Calle 2',
+      distancia: '2 km',
+    },
+    {
+      nombreRestaurante: 'Restaurante 3',
+      imagenRestaurante: 'https://picsum.photos/400/300',
+      etiquetas: ['japonesa', 'sushi'],
+      calificacion: 5.0,
+      direccion: 'Calle 3',
+      distancia: '3 km',
+    },
+  ];
+}
